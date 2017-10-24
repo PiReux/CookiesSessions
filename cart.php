ad4a49f45285f4ea791621e60d1a9f16ac3ad18b
Pierrick Reux <pierrick.reux@gmail.com>
2017-10-24 14:41:46 +0200

    connecion, basket and deconnection available by session

--- cart.php
+++ cart.php
@@ -1,7 +1,45 @@
 <?php require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
-      TODO : Display shopping cart items from $_COOKIES here.
-    </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Pecan nuts</h3>
+                    <p>Nombre dans votre panier:</p>
+                    <p><?php echo $_SESSION['pecanNuts'];?></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Chocolate chips</h3>
+                    <p>Nombre dans votre panier:</p>
+                    <p><?php echo $_SESSION['chocolateChips'];?></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Chocolate cookie</h3>
+                    <p>Nombre dans votre panier:</p>
+                    <p><?php echo $_SESSION['chocolateCookie'];?></p>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>M&M's&copy; cookies</h3>
+                    <p>Nombre dans votre panier:</p>
+                    <p><?php echo $_SESSION['MMsCookies'];?></p>
+                </figcaption>
+            </figure>
+        </div>
 </section>
 <?php require 'inc/foot.php'; ?>
